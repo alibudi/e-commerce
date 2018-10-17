ef045178097bc607f8bddcf43c8d2f29de56ac28
alibudi <alibudipurnomo@gmail.com>
2018-10-17 17:30:52 +0700

    pertama

---
+++ src/java/controller/CategoriesController.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Category;
+
+/**
+ *
+ * @author x201
+ */
+public class CategoriesController extends HttpServlet {
+
+    private String list_action;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = response.getWriter()) {
+                String action = request.getParameter("action");
+                if(action==null){
+                    action= "list";
+                }
+                try {
+                    switch (action) {
+                    case "new":
+                        break;
+                    case "create":
+                        break;
+                    case "delete":
+                        break;
+                    case "edit":
+                        break;
+                    case "update":
+                        break;
+                    default:
+                        listCategories(request, response);
+                        break;
+                    }
+                } 
+            catch (SQLException ex) {
+                throw new ServletException(ex);
+            }
+        }
+    }
+
+    
+    private void listCategories(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        Category c= new Category();
+        List<Category> categories = c.all();
+        request.setAttribute("categories", categories);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("categories/list.jsp");
+        dispatcher.forward(request, response);
+        
+    }
+    
+     private void createCategories(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        String category_id = request.getParameter("category_id");
+ 
+        Category category = new Category();
+        category.setId(id);
+        category.setName(name);
+        category.setParentId(category_id);
+        if (category.create()){
+            String message = "new person added";                    
+          
+            response.sendRedirect("categories?action="+list_action);
+        }
+        else{
+            String message = "new category failed to add";
+           
+            String add_action = null;
+            request.getRequestDispatcher("categories?action="+add_action).include(request, response);
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
